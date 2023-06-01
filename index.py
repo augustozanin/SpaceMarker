c478f019a6af263eccd3f4ec919b3155a224c0ca
Augusto Zanin <augustozann@gmail.com>
2023-06-01 15:48:52 -0300

    Começo do projeto

---
+++ index.py
@@ -0,0 +1,17 @@
+import pygame
+
+pygame.init()
+
+tamanho = (800,600)
+display = pygame.display.set_mode(tamanho)
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    pygame.display.update()
+
+pygame.quit()
