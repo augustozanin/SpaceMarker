4bc2edeb052db280be8a849cba88509b31e253c9
Augusto Zanin <augustozann@gmail.com>
2023-06-01 16:06:48 -0300

    Implementados: Trilha sonora e fundo

--- index.py
+++ index.py
@@ -2,16 +2,18 @@ import pygame
 
 pygame.init()
 
-tamanho = (800,600)
+tamanho = (600, 600)
 display = pygame.display.set_mode(tamanho)
-
+pygame.mixer.music.load("Space.mp3")
+pygame.mixer.music.play(-1)
 running = True
-
+fundo = pygame.image.load("bg.jpg")
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    
+
+    display.blit(fundo, (0, 0))
     pygame.display.update()
 
 pygame.quit()
